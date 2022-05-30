e83d52d3ff0c4737d8c52ece37d67f25da9a4092
Vishnukumar2233 <106506540+Vishnukumar2233@users.noreply.github.com>
2022-05-30 20:46:11 +0530

    Update alpha_check.py

--- alpha_check.py
+++ alpha_check.py
@@ -1,4 +1,4 @@
-Write a Python program to check whether an alphabet is a vowel or consonant.
+##Write a Python program to check whether an alphabet is a vowel or consonant.
 
 apha=input("Enter only single character  ")
 if apha=="a" or apha=="e" or apha=="i" or apha=="o" or apha=="u" or apha=="A" or apha=="E" or apha=="I" or apha=="O" or apha=="U":
